1ea08488984645639ed0f05cedb10659ebd26b27
Teddy O'Brien <tedfobrien@icloud.com>
2024-07-24 22:26:05 -0400

    nvim 2

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -3,8 +3,11 @@ return {
   version = "*",
   lazy = false,
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  }
   config = function()
     require("nvim-tree").setup {}
   end,
