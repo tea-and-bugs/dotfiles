e8ce4937381673e8f8b54a3c8fc5166a28320d11
tedster-08 <75181949+tedster-08@users.noreply.github.com>
2024-09-06 23:48:28 -0400

    09-06-24

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,5 +5,33 @@ return {
 	},
 	config = function()
 		pcall(require('nvim-treesitter.install').update { with_sync = true })
+		pcall(require('nvim-treesitter.configs').setup {
+
+			modules = {},
+			sync_install = false,
+			ignore_install = {},
+
+
+			ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vim', 'ruby' },
+			auto_install = false,
+
+			highlight = { enable = true },
+			indent = { enable = true, disable = { 'python' } },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = '<c-space>',
+					node_incremental = '<c-space>',
+					scope_incremental = '<c-s>',
+					node_decremental = '<M-space>',
+				},
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+				}
+			}
+		})
 	end,
 }
